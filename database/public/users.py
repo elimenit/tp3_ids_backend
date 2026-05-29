9824af0c92fe244977f8b09e3f22c48c90aaf0d9
longotiziano <longo.tiziano.arg@gmail.com>
2026-05-29 13:40:53 -0300

    cambio en el manejo de las conexiones de la base de datos para que SIEMPRE se cierren y no queden colgadas

--- database/public/users.py
+++ database/public/users.py
@@ -1,94 +1,75 @@
 from database.db import get_connection
 
-def db_login(email: str, password: str):
+def db_login(email: str, password: str) -> dict:
     """Login de usuario.\n
     """
-    conn = get_connection()
-    if conn:
+    with get_connection() as conn:
         with conn.cursor(dictionary=True) as cursor:
             query = "SELECT id FROM users WHERE email = %s AND password = %s"
             cursor.execute(query, (email, password))
             user = cursor.fetchone()
             if not user:
                 raise Exception("Credenciales inválidas")
-    else:
-        raise Exception("Error de conexión a la base de datos")
-    return user['id']
-
+            
+    return user # type: ignore
+    
 def db_get_user(id: int):
     """Obtener usuario.\n
     """
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    query = "SELECT id, name, email, category, created_at FROM users WHERE id = %s"
-    cursor.execute(query, (id, ))
-    user = cursor.fetchone()
-
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            query = "SELECT id, name, email, category, created_at FROM users WHERE id = %s"
+            cursor.execute(query, (id, ))
+            user = cursor.fetchone()
+            
     if user is None:
-        cursor.close()
-        conn.close()
         raise Exception("El usuario no existe")
-    
-    cursor.close()
-    conn.close()
     
     return user
 
-def db_create_user(name: str, email: str, password: str)-> None:
+def db_create_user(name: str, email: str, password: str) -> int:
     """ Inserta un usuario.\n
     Devuelve el id del usuario creado 
     """
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT email from users WHERE email = %s", (email, ))
-    user = cursor.fetchone()
-    if user is not None:
-        cursor.close()
-        conn.close()
-        raise Exception("EL usuario ya existe")
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT email from users WHERE email = %s", (email, ))
+            user = cursor.fetchone()
+            if user is not None:
+                raise Exception("El usuario ya existe")
+            
+            query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
+            cursor.execute(query, (name, email, password))
+            conn.commit()
+            id_user = cursor.lastrowid
     
-    query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
-    cursor.execute(query, (name, email, password))
-    conn.commit()
-
-    id_user = cursor.lastrowid
-    
-    cursor.close()
-    conn.close()
-    return id_user
+    return id_user # type: ignore
 
 def db_update_user(id: int, name: str, password: str)-> None:
-    conn = get_connection()
-    cursor = conn.cursor()
-    query = """
-    UPDATE users
-    SET name = %s, password = %s
-    WHERE id = %s;
-    """
-    cursor.execute(query, (name, password, id))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            query = """
+            UPDATE users
+            SET name = %s, password = %s
+            WHERE id = %s;
+            """
+            cursor.execute(query, (name, password, id))
+            conn.commit()
 
 def db_delete_user(id: int)-> bool:
     """
     Elimina un usuario por su id.\n
     """
-    conn = get_connection()
-    cursor = conn.cursor()
-    
-    cursor.execute("SELECT email FROM users WHERE id = %s", (id, ))
-    email = cursor.fetchone()
-    print(email)
-    if not email:
-        raise Exception("Usuario no registrado!")
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT email FROM users WHERE id = %s", (id, ))
+            email = cursor.fetchone()
+            print(email)
+            if not email:
+                raise Exception("Usuario no registrado!")
 
-    query = "DELETE FROM users WHERE id = %s"
-    cursor.execute(query, (id,))
-    conn.commit()
+            query = "UPDATE users SET status = 'inactive' WHERE id = %s"
+            cursor.execute(query, (id,))
+            conn.commit()
     
-    cursor.close()
-    conn.close()
-    return email
+    return email # type: ignore
