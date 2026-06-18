764684e06397e2673ba99b4b128c421924e2cd36
longotiziano <longo.tiziano.arg@gmail.com>
2026-06-18 12:39:48 -0300

    eliminado del dashboard de deliveries

--- routers/admin/dashboards.py
+++ routers/admin/dashboards.py
@@ -4,7 +4,6 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from utils.helpers import _is_admin, _get_date_range
 from database.admin.dashboard import (
     db_get_reservations_dashboard,
-    db_get_delivery_dashboard,
     db_get_reviews_dashboard,
 )
 
@@ -20,19 +19,6 @@ def get_reservations_dashboard():
     inicio, fin = _get_date_range()
     data = db_get_reservations_dashboard(inicio, fin)
     return jsonify(data), 200
-
-
-@adm_bp_dashboards.get("/deliveries")
-@jwt_required()
-def get_deliveries_dashboard():
-    user_id = int(get_jwt_identity())
-    if not _is_admin(user_id):
-        return jsonify({"error": "Acceso no autorizado"}), 403
-
-    inicio, fin = _get_date_range()
-    data = db_get_delivery_dashboard(inicio, fin)
-    return jsonify(data), 200
-
 
 @adm_bp_dashboards.get("/reviews")
 @jwt_required()
